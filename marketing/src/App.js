2dafffb3fdbbbe74ad373a9c4138ecadfbaf47f6
Anubhav Bhardwaj <anubhav.bhardwaj@nagarro.com>
2025-06-11 11:27:22 +0530

    setting-production-prefix-option

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({ productionPrefix: "ma" });
 
 export const App = () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <Router>
         <Switch>
           <Route exact path="/" component={Landing} />
